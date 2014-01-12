27f1911685a5efd06014b0055ecf171c9d6e1f03
Michael Dippery <michael@monkey-robot.com>
2014-01-11 23:15:24 -0800

    Remove unused bash completion scripts
    A few years ago I attempted to write bash completion scripts for
    fabric and rake; the endeavour did not go so well, and I ended up
    never actually using them.

--- bash_completion.d/rake.sh
+++
@@ -1,9 +0,0 @@
-_complete_rake() {
-  COMPREPLY=()
-  if [ -e ./Rakefile ]; then
-    local targets=$(rake -T | awk 'FNR>1' | sed -e 's/^rake //' -e 's/#.*$//' -e 's/[ \t]*$//')
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-    COMPREPLY=( $(compgen -W "${targets}" -- ${cur}) )
-  fi
-}
-complete -o bashdefault -o default -F _complete_rake rake
