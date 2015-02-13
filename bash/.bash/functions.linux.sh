a5470a501ae25f9bba6be81d4378002213e9cecb
Michael Dippery <michael@monkey-robot.com>
2015-02-13 14:51:25 -0800

    bash: Recreate `otool -L` on Linux

---
+++ bash/.bash/functions.linux.sh
@@ -0,0 +1,12 @@
+function otool {
+  if [ $# -lt 1 -o $1 != '-L' ]; then
+    local cmd='ldd'
+    if [ $# -gt 0 ]; then
+      cmd="$cmd $*"
+    fi
+    echo "I think you mean \`$cmd\`" 2>&1
+    return 1
+  fi
+  shift
+  ldd "$1"
+}
--- bash/.bash/functions.sh
+++ bash/.bash/functions.sh
@@ -56,4 +56,5 @@ function wjava {
   grep -l "$1" *.jar
 }
 
+[ -r "${BASH}/functions.${OS}.sh" ] && source "${BASH}/functions.${OS}.sh"
 [ -r "${BASH}/functions.user.sh" ] && source "${BASH}/functions.user.sh"
