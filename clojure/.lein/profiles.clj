f41b91163cc43db97238ec50e139a95e3a7c9e9c
Michael Dippery <michael@monkey-robot.com>
2015-09-02 11:31:03 -0700

    clojure: Install lein-bin plugin

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.7.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
-        :plugins [[lein-cloverage "1.0.2"]]}}
+        :plugins [[lein-bin "0.3.5"]
+                  [lein-cloverage "1.0.2"]]}}
