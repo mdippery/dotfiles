9290ddba1db1daed1ba69e2cbc079c1b6181f2b9
Michael Dippery <michael@monkey-robot.com>
2015-09-02 10:37:08 -0700

    clojure: Use humane-test-output for clojure.test

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,1 +1,4 @@
-{:user {:plugins [[lein-cloverage "1.0.2"]]}}
+{:user {:dependencies [[pjstadig/humane-test-output "0.7.0"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        :plugins [[lein-cloverage "1.0.2"]]}}
