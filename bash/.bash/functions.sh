de21c21773e10b76f148126b7c8dd384d020e2e1
Michael Dippery <michael@monkey-robot.com>
2015-10-26 01:55:01 -0700

    bash: `greet`: Don't show time when using `uptime`

--- bash/.bash/functions.sh
+++ bash/.bash/functions.sh
@@ -50,7 +50,7 @@ function greet {
   elif hash ddate 2>/dev/null; then
     ddate | cowthink | lolcat
   elif ! alias cowthink 2>/dev/null; then
-    uptime | cowthink -n | lolcat
+    uptime | tail -c +8 | cowthink -n | lolcat
   else
     echo "$(hostname -s)?" | cowthink | lolcat
   fi
