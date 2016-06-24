7a6d6b12b78eddccb2768884f867a80273decccc
Michael Dippery <michael@monkey-robot.com>
2016-06-24 13:43:32 -0700

    bash: `cpuinfo` function

--- bash/.bash/plat/linux/functions.sh
+++ bash/.bash/plat/linux/functions.sh
@@ -1,3 +1,7 @@
+function cpuinfo {
+  lscpu | command egrep 'Thread|Core|Socket|^CPU\('
+}
+
 function netinfo {
   ifconfig eth0 | command grep -E -o 'inet addr:[^ ]+' | awk -F: '{print $2}'
   ifconfig eth0 | command grep -E -o 'HWaddr .+$' | awk '{print $2}'
