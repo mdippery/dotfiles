7acb0a19398189c94ada64ae758be6b0b7871b28
Michael Dippery <michael@monkey-robot.com>
2016-10-17 10:30:50 -0700

    python: Source /etc/pythonstart if it exists
    My work machine has a handy Python startup file at /etc/pythonstart, so
    read that, too, if it exists.

--- python/.config/python/startup.py
+++ python/.config/python/startup.py
@@ -1,5 +1,11 @@
+import os.path
 import sys
+
 sys.ps1 = "\033[030;1m>>>\033[0m "
 sys.ps2 = "\033[030;1m...\033[0m "
 
-del sys
+if os.path.exists("/etc/pythonstart"):
+    with open("/etc/pythonstart", "r") as fh:
+        exec(fh.read())
+
+del fh, sys
