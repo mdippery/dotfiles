ba08a03e0d2ae73c088ab1cab315b6d471709b39
Michael Dippery <michael@monkey-robot.com>
2016-11-01 10:00:46 -0700

    python: Ignore ANSI codes when calculating prompt length
    Readline includes the ANSI codes when calculating the length of the
    prompt, which means that the display when cycling through
    previously-entered lines is displayed incorrectly (it doesn't completely
    delete the line in some cases). More info is available at [1], but
    suffice it to say that surrounding the ANSI codes in the Readline
    ignore/unignore control characters (\001 and \002) solves the problem.
    
      [1] https://stackoverflow.com/a/9468954/28804

--- python/.config/python/startup.py
+++ python/.config/python/startup.py
@@ -1,8 +1,8 @@
 import os.path
 import sys
 
-sys.ps1 = "\033[030;1m>>>\033[0m "
-sys.ps2 = "\033[030;1m...\033[0m "
+sys.ps1 = "\001\033[030;1m\002>>>\001\033[0m\002 "
+sys.ps2 = "\001\033[030;1m\002...\001\033[0m\002 "
 
 if os.path.exists("/etc/pythonstart"):
     with open("/etc/pythonstart", "r") as fh:
