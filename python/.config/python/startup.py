de51a1f5928bd9dca592d3755d639dcf8ed21b53
Michael Dippery <michael@monkey-robot.com>
2016-11-04 12:11:55 -0700

    python: Save interpreter history
    Inspired by [1], this startup file also saves your history so you can
    use the up and down arrows to cycle through lines. It should work in
    both Python 2 and Python 3.
    
      [1] https://docs.python.org/2/tutorial/interactive.html

--- python/.config/python/startup.py
+++ python/.config/python/startup.py
@@ -1,11 +1,23 @@
+import atexit
 import os.path
+import readline
+import rlcompleter
 import sys
+
+HISTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), 'history'))
 
 sys.ps1 = "\001\033[030;1m\002>>>\001\033[0m\002 "
 sys.ps2 = "\001\033[030;1m\002...\001\033[0m\002 "
 
-if os.path.exists("/etc/pythonstart"):
-    with open("/etc/pythonstart", "r") as fh:
-        exec(fh.read())
+readline.parse_and_bind('tab: complete')
 
-del fh, sys
+def save_history(path=HISTORY):
+    import readline
+    readline.write_history_file(path)
+
+if os.path.exists(HISTORY):
+    readline.read_history_file(HISTORY)
+
+atexit.register(save_history)
+
+del atexit, HISTORY, os, readline, rlcompleter, save_history, sys
