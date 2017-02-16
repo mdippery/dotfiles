427b2c35deb055410d405c22797d8525e55eb47d
Michael Dippery <michael@monkey-robot.com>
2017-02-16 10:50:41 -0800

    bash: Alphabetize aliases

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -1,6 +1,6 @@
+alias bc='bundle console'
 alias be='bundle exec'
 alias bi='bundle install --path=.bundle'
-alias bc='bundle console'
 alias clj='lein repl'
 alias ctags='ctags -f .tags'
 alias d='pwd'
