2bdd6d9f94c514086abd3876f0e46ed858a5ca69
Michael Dippery <michael@monkey-robot.com>
2017-04-18 10:44:34 -0700

    clojure: Ultra plugin

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,5 +1,4 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.7.0"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]
         :plugins [[lein-bin "0.3.5"]
-                  [lein-cloverage "1.0.2"]]}}
+                  [lein-cloverage "1.0.2"]
+                  [venantius/ultra "0.5.1"]]}}
