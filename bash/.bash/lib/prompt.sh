816e8a941f755794ba08a9773f501e83c46843c7
Michael Dippery <michael@monkey-robot.com>
2018-06-05 12:01:56 -0700

    bash: Refactor prompt segments into functions

--- bash/.bash/lib/prompt.sh
+++ bash/.bash/lib/prompt.sh
@@ -4,34 +4,40 @@ function _ps1_str {
   echo -ne "\[$(tput setaf $color)\]$s\[$(tput sgr0)\] \[$(tput setaf 0)$(tput bold)\]>\[$(tput sgr0)\] "
 }
 
-function _ps1 {
-  local last_exit=$?
-
-  local pushed_dirs git_branch venv exit_code
-
-  if (( $(dirs -v | wc -l) > 1 )); then
-    pushed_dirs="\[$(tput setaf 1)\]+\[$(tput sgr0)\] "
-  fi
-
-  if [ -n "$VIRTUAL_ENV" ]; then
-    venv="\[$(tput setaf 6)\]\342\226\266\[$(tput sgr0)\] "
-  fi
-
+function _ps1_git_prompt {
   if git rev-parse --git-dir >/dev/null 2>&1; then
-    git_branch='↯'
+    local git_branch='↯'
     # TODO: Detect untracked files
     if git diff-index --quiet HEAD; then
-      git_branch="\[$(tput setaf 0)$(tput bold)\]${git_branch}\[$(tput sgr0)\] "
+      echo -ne "\[$(tput setaf 0)$(tput bold)\]${git_branch}\[$(tput sgr0)\] "
     else
-      git_branch="\[$(tput setaf 2)\]${git_branch}\[$(tput sgr0)\] "
+      echo -ne "\[$(tput setaf 2)\]${git_branch}\[$(tput sgr0)\] "
     fi
   fi
+}
 
+function _ps1_exit_code {
+  local last_exit=$1
   if (( $last_exit == 0 )); then
-    exit_code="\[$(tput setaf 2)$(tput bold)\]•\[$(tput sgr0)\] "
+    echo -ne "\[$(tput setaf 2)$(tput bold)\]•\[$(tput sgr0)\] "
   else
-    exit_code="\[$(tput setaf 1)$(tput bold)\]•\[$(tput sgr0)\] "
+    echo -ne "\[$(tput setaf 1)$(tput bold)\]•\[$(tput sgr0)\] "
   fi
+}
 
-  export PS1="${exit_code}${pushed_dirs}${venv}${git_branch}$(_ps1_str \\W 4)"
+function _ps1_pushed_dirs {
+  if (( $(dirs -v | wc -l) > 1 )); then
+    echo -ne "\[$(tput setaf 1)\]+\[$(tput sgr0)\] "
+  fi
+}
+
+function _ps1_virtual_env {
+  if [ -n "$VIRTUAL_ENV" ]; then
+    echo -ne "\[$(tput setaf 6)\]\342\226\266\[$(tput sgr0)\] "
+  fi
+}
+
+function _ps1 {
+  local last_exit=$?
+  export PS1="$(_ps1_exit_code $last_exit)$(_ps1_pushed_dirs)$(_ps1_virtual_env)$(_ps1_git_prompt)$(_ps1_str \\W 4)"
 }
