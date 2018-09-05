15421329a8cc4b7f46b86dda9159f83fc1288cf4
Michael Dippery <michael@monkey-robot.com>
2018-09-05 09:54:54 -0700

    bash: Read arbitrary paths-style file with `read-paths` function

--- bash/.bash/lib.sh
+++ bash/.bash/lib.sh
@@ -9,29 +9,12 @@ function dotbash {
   esac
 }
 
-function manpaths-helper {
-  export MANPATHS_FILE="$(dotbash sys)/manpaths"
-  if [ -r "$MANPATHS_FILE" ]; then
-    tr '\n' ':' < "$MANPATHS_FILE" | sed 's/:$//'
-  else
-    echo $MANPATH
-  fi
-}
-
-function paths-helper {
-  export PATHS_FILE="$(dotbash sys)/paths"
-  if [ -r "$PATHS_FILE" ]; then
-    tr '\n' ':' < "$PATHS_FILE" | sed 's/:$//'
-  else
-    echo $PATH
-  fi
-}
-
 function onoe {
   echo $* 1>&2
 }
 
 source "$(dotbash)/lib/dots.sh"
+source "$(dotbash)/lib/paths.sh"
 
 # Recreate behavior of GNU `readlink` on OS X
 # Some of the bash config scripts expect `readlink -f` to work on OS X
---
+++ bash/.bash/lib/paths.sh
@@ -0,0 +1,21 @@
+function manpaths-helper {
+  export MANPATHS_FILE="$(dotbash sys)/manpaths"
+  if [ -r "$MANPATHS_FILE" ]; then
+    read-paths $MANPATHS_FILE
+  else
+    echo $MANPATH
+  fi
+}
+
+function paths-helper {
+  export PATHS_FILE="$(dotbash sys)/paths"
+  if [ -r "$PATHS_FILE" ]; then
+    read-paths $PATHS_FILE
+  else
+    echo $PATH
+  fi
+}
+
+function read-paths {
+  tr '\n' ':' < $1 | sed 's/:$//'
+}
