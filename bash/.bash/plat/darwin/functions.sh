5a9889a54a730c5e3fd4f43db29fb5faea8faa55
Michael Dippery <michael@monkey-robot.com>
2018-11-16 15:41:26 -0800

    bash: Move `res` to script
    I call this from my vim config, which can't utilize functions or
    aliases.

--- bash/.bash/plat/darwin/functions.sh
+++ bash/.bash/plat/darwin/functions.sh
@@ -8,11 +8,3 @@ function ldd {
 function rebuild-openwith {
   /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 }
-
-# Displays desktop resolution
-function res {
-  osascript -e 'tell application "Finder" to get bounds of window of desktop' \
-    | cut -d, -f 3,4 \
-    | sed 's/, /x/' \
-    | tr -d ' '
-}
