7157b7dc8889d2af10c02a34e0daa7d13b4dcd30
Michael Dippery <michael@monkey-robot.com>
2019-04-17 10:38:33 -0700

    bash: New function: `install-missing-headers`
    Sometimes after an Xcode upgrade, header files are not installed to
    /usr/include as they should be. This function installs them manually.

--- bash/.bash/plat/darwin/functions.sh
+++ bash/.bash/plat/darwin/functions.sh
@@ -1,3 +1,10 @@
+# Sometimes Xcode upgrades don't install header files in /usr/include properly
+function install-missing-headers {
+  local dir=/Library/Developer/CommandLineTools/Packages
+  local pkg=$(ls -t1 $dir | head -n 1)
+  open "$pkg"
+}
+
 function ldd {
   onoe 'I think you mean `otool [-L]`'
   return 1
