c25e24e20a7320f4bd137e405ad873c70ca9930c
Michael Dippery <michael@monkey-robot.com>
2019-08-12 10:44:25 -0700

    python: Print UTF-8 path properly on Python > 2.7.15

--- python/.config/python/startup.py
+++ python/.config/python/startup.py
@@ -12,6 +12,9 @@ HISTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), HISTORY_BASE))
 if IS_PY3:
     sys.ps1 = "\u27a5  "
     sys.ps2 = "\u22ef  "
+elif sys.version_info > (2,7,15):
+    sys.ps1 = u"\u27a5  ".encode("utf-8")
+    sys.ps2 = u"\u22ef  ".encode("utf-8")
 else:
     sys.ps1 = "\001\033[030;1m\002>>>\001\033[0m\002 "
     sys.ps2 = "\001\033[030;1m\002...\001\033[0m\002 "
