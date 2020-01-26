50e6d31c8e287c2e9e07b16665d124b03d49da08
Michael Dippery <michael@monkey-robot.com>
2020-01-26 14:32:41 -0800

    bash: New, fancier greeting for macOS
    Inspired by the default Terminal greeting in KISS Linux [1]. Eventually
    I'll do one for Linux, once I'm using Linux regularly again; for now,
    Linux can just use my old greeting.
    
      [1] https://getkiss.org/pages/scrots

--- bash/.bash/plat/darwin/functions.sh
+++ bash/.bash/plat/darwin/functions.sh
@@ -1,3 +1,54 @@
+function _free_memory {
+  local pages=$(\
+    vm_stat \
+      | grep 'Pages free:' \
+      | awk -F : '{ print $2 }' \
+      | tr -d ' ' \
+      | tr -d '.' \
+  )
+  expr $(($pages * 4 / 1024))
+}
+
+function _hardware_info {
+  system_profiler SPHardwareDataType \
+    | grep "$1" \
+    | cut -d : -f 2 \
+    | sed 's/^ //g'
+}
+
+function _total_memory {
+  expr $(sysctl -n hw.memsize) / $((1024**2))
+}
+
+function greet {
+  local indent=$(printf '\t\t    ')
+  local grey="$(tput setaf 0)$(tput bold)"
+  local reset=$(tput sgr0)
+  local user="$(tput setaf 2)"
+  cat <<EOM
+${indent}${user}$(whoami)@$(system_profiler SPSoftwareDataType | grep 'Computer Name' | awk '{ print $3 }')${reset}
+${indent}${grey}os${reset}      macOS $(sw_vers -productVersion)
+${indent}${grey}host${reset}    $(_hardware_info 'Model Identifier')
+${indent}${grey}cpu${reset}     $(_hardware_info 'Number of Processors') $(_hardware_info 'Processor Speed') $(_hardware_info 'Processor Name')
+${indent}${grey}kernel${reset}  Darwin $(uname -r)
+${indent}${grey}uptime${reset}  $(system_profiler SPSoftwareDataType | grep 'Time since boot' | cut -d : -f 2,3 | sed 's/^ //g')
+${indent}${grey}memory${reset}  $(_free_memory) MB / $(_total_memory) MB
+${indent}${grey}load${reset}    $(uptime | awk -F , '{ print $NF }' | awk -F : '{ print $NF }' | sed 's/^ //g')
+${indent}${grey}shell${reset}   $SHELL
+EOM
+  echo -e "\033[9A"
+  lolcat <<EOM
+          .:'
+      __ :'__
+   .'\`__\`-'__\`\`.
+  :__________.-'
+  :_________:
+   :_________\`-;
+    \`.__.-.__.'
+
+EOM
+}
+
 # Sometimes Xcode upgrades don't install header files in /usr/include properly
 function install-missing-headers {
   local dir=/Library/Developer/CommandLineTools/Packages
