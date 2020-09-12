7b2135d4722fb5ae54fb9d64457c2e990c8f850a
Michael Dippery <michael@monkey-robot.com>
2020-09-12 16:17:45 -0700

    bash: Source completion files in Homebrew's profile.d
    JFC, why does Homebrew always change things for the sake of changing
    things. What was a nice, simple tool a decade ago has become a
    monstrosity now that more "hackers" have gotten involved in it. Anyway,
    Homebrew now installs completion scripts into /usr/local/etc/profile.d
    instead of /usr/local/etc/bash_completion.d, so we have to source
    scripts from there, too.

--- bash/.bash/completion.sh
+++ bash/.bash/completion.sh
@@ -19,18 +19,25 @@ if [ -d $bash_completion_d ]; then
   done
 fi
 
-bash_completion_d="$(brew --prefix)/etc/bash_completion.d"
-if [ -d $bash_completion_d ]; then
-  for f in $(find $bash_completion_d -mindepth 1 -not -name .gitignore); do
-    # Skip git-prompt.sh -- I don't want to source that
-    if [ $(basename $f) != 'git-prompt.sh' ]; then
-      source $f
-    fi
-  done
-fi
+bash_completion_d=(bash_completion profile)
+for d in ${bash_completion_d[@]}; do
+  d="$(brew --prefix)/etc/${d}.d"
+  if [ -d $d ]; then
+    for f in $(find $d -mindepth 1 -not -name .gitignore); do
+      # Skip git-prompt.sh -- I don't want to source that
+      if [ $(basename $f) != 'git-prompt.sh' ]; then
+        source $f
+      fi
+    done
+  fi
+done
+
+bash_completion_d="$(brew --prefix)/etc/profile.d/bash_completion.sh"
 
 unset brew
 unset brew_completion
 unset bash_completion_d
+unset d
+unset f
 
 [ -r "$(dotbash)/completion.user.sh" ] && source "$(dotbash)/completion.user.sh"
