b973b6b8db9032676703d1e30a20289b2630b9d6
Michael Dippery <michael@monkey-robot.com>
2020-10-07 10:57:45 -0700

    bash: Alias `pydoc` to `python -m pydoc`
    I hate how newer versions of Python are getting rid of the scripts in
    favor of invoking the Python module directly. Not sure why that makes
    sense, and it's definitely a lot harder to call built-in scripts.

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -31,6 +31,7 @@ alias p='pushd'
 alias path='echo $PATH | tr ":" "\n"'
 alias pe='printenv'
 alias pyack='ack --type=python'
+alias pydoc='python -m pydoc'
 alias pytree="tree -I '__pycache__|*.egg-info'"
 alias Q='echo $OLDPWD'
 alias q='cd -'
