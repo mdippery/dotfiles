fe81a32dd19cf221c7c13a93add13bea4a2d2c92
Michael Dippery <michael@monkey-robot.com>
2022-01-04 09:24:50 -0800

    clojure: Install lein-ancient plugin globally

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.9.0"]]
-        :plugins [[lein-cloverage "1.1.1"]]}}
+        :plugins [[lein-cloverage "1.1.1"]
+                  [lein-ancient "0.7.0"]]}}
