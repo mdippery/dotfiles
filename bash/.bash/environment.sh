bbafbf79a0eb393de8cb9bbdc78d8581307542af
Michael Dippery <michael@monkey-robot.com>
2022-12-09 14:55:16 -0800

    bash: Set $GOPATH
    It's obnoxious that Go just plops a _visible_ directory as ~/go, but
    what can you expect from Google?

--- bash/.bash/environment.sh
+++ bash/.bash/environment.sh
@@ -36,6 +36,7 @@ export CUCUMBER_PUBLISH_QUIET=true
 export DJANGO_DEBUG=true
 export FIGNORE='.terraform:.DS_Store'
 export GHCUP_USE_XDG_DIRS=true
+export GOPATH="${HOME}/Library/Application Support/go"
 export GPG_TTY=$(tty)
 export GREP_OPTIONS='--color'
 export LESSOPEN="|${XDG_BIN_HOME}/lessopen %s"
