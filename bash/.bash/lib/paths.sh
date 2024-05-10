a7f051773410db6bb9566eb6ccfab58e36861169
Michael Dippery <michael@monkey-robot.com>
2024-05-10 10:24:28 -0700

    bash: Remove /Library/TeX/texbin from path
    This actually gets added to the path automatically by the MacTeX
    installer, by dropping a file into /etc/paths.d, so adding it manually
    is not necessary.
    
    (I think the same is true of /opt/X11/bin, should I never get around to
    installing that again, but I'll leave that path there for now.)

--- bash/.bash/lib/paths.sh
+++ bash/.bash/lib/paths.sh
@@ -1,6 +1,6 @@
 function paths-helper {
   local BREW RAW_PATH
-  local homebrew texbin x11bin whichbin gettext
+  local homebrew x11bin whichbin gettext
 
   if [ $(uname -m) = arm64 ]; then
     homebrew=/opt/homebrew/bin
@@ -11,7 +11,6 @@ function paths-helper {
 
   [ -d /Applications/MacVim.app/Contents/bin ] && macvim=/Applications/MacVim.app/Contents/bin
 
-  [ -d /Library/TeX/texbin ] && texbin=/Library/TeX/texbin
   [ -d /opt/X11/bin ] && x11bin=/opt/X11/bin
   $BREW --prefix --installed gnu-which >/dev/null 1>&1 gnu-which && whichbin=$($BREW --prefix gnu-which)/libexec/gnubin
   $BREW --prefix --installed gettext >/dev/null 1>&1 gettext && gettext=$($BREW --prefix gettext)/bin
@@ -21,7 +20,6 @@ $XDG_BIN_HOME
 $(dirname $XDG_BIN_HOME)/libexec/git
 $macvim
 $homebrew
-$texbin
 $x11bin
 $whichbin
 $gettext
