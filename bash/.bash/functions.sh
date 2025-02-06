814df691b6f6ba7a601983d32da53c9b635d2c37
Michael Dippery <michael@monkey-robot.com>
2025-02-06 15:00:30 -0800

    bash: Use `column` in `qh` instead of manual alignment

--- bash/.bash/functions.sh
+++ bash/.bash/functions.sh
@@ -328,21 +328,21 @@ function pyvenv {
 
 # Show quick bash nav help
 function qh {
-  cat <<EOS
-ctrl-a          Move to beginning of line
-ctrl-e          Move to end of line
-meta-b          Move back one word
-meta-f          Move forward one word
-ctrl-x ctrl-e   Open command line in \$EDITOR
-ctrl-u          Delete from cursor to beginning of line
-ctrl-k          Delete from cursor to end of line
-ctrl-w          Delete word before cursor
-alt-d           Delete word after cursor
-ctrl-d          Delete current character
-ctrl-t          Transpose current character with previous
-alt-t           Transpose current word with previous
-alt-l           Make lowercase word after cursor
-alt-u           Make uppercase word after cursor
+  cat <<EOS | column -t -s,
+ctrl-a,Move to beginning of line
+ctrl-e,Move to end of line
+meta-b,Move back one word
+meta-f,Move forward one word
+ctrl-x ctrl-e,Open command line in \$EDITOR
+ctrl-u,Delete from cursor to beginning of line
+ctrl-k,Delete from cursor to end of line
+ctrl-w,Delete word before cursor
+alt-d,Delete word after cursor
+ctrl-d,Delete current character
+ctrl-t,Transpose current character with previous
+alt-t,Transpose current word with previous
+alt-l,Make lowercase word after cursor
+alt-u,Make uppercase word after cursor
 EOS
 }
 
