58c3808094bf8d4647af109a21f18b1120386cc2
Michael Dippery <michael@monkey-robot.com>
2026-03-04 13:38:58 -0800

    claude: Track Claude config in repository

---
+++ claude/.claude/status.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+input=$(cat)
+
+yellow=$(tput setaf 3)
+teal=$(tput setaf 6)
+reset=$(tput sgr0)
+
+sep=" • "
+
+cwd=$(basename $(echo "$input" | jq -r .workspace.current_dir))
+model=$(echo "$input" | jq -r .model.display_name)
+branch=$(git branch --show-current)
+
+# echo "📂 $cwd • ${yellow} $branch${reset} • 🤖 ${teal}${model}${reset}"
+echo -n "📂 $cwd"
+echo -n "$sep"
+echo -n "${yellow} $branch${reset}"
+echo -n "$sep"
+echo -n "🤖 ${teal}${model}${reset}"
