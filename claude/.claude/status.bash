d610f168021bcedb930eec43e3a834994ab5f5ad
Michael Dippery <michael@monkey-robot.com>
2026-03-26 09:52:02 -0700

    claude: Maybe show usage in status line (but not really)
    It might be useful so I want to keep the code in the script, but right
    now it's a bit of information overload.

--- claude/.claude/status.bash
+++ claude/.claude/status.bash
@@ -3,6 +3,7 @@
 input=$(cat)
 
 yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
 teal=$(tput setaf 6)
 reset=$(tput sgr0)
 
@@ -10,11 +11,13 @@ sep=" • "
 
 cwd=$(basename $(echo "$input" | jq -r .workspace.current_dir))
 model=$(echo "$input" | jq -r .model.display_name)
+usage=$(echo "$input" | jq -r .rate_limits.five_hour.used_percentage)
 branch=$(git branch --show-current)
 
-# echo "📂 $cwd • ${yellow} $branch${reset} • 🤖 ${teal}${model}${reset}"
 echo -n "📂 $cwd"
 echo -n "$sep"
 echo -n "${yellow} $branch${reset}"
 if [ $(tput cols) -gt 100 ]; then echo -n "$sep"; else echo; fi
+# echo -n "⏱️ ${blue}${usage}%${reset}"
+# echo -n "$sep"
 echo -n "🤖 ${teal}${model}${reset}"
