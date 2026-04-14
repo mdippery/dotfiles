d6afc95c99117f31cb9e4eee8adc48cb0fa6906a
Michael Dippery <michael@monkey-robot.com>
2026-04-14 13:55:57 -0700

    fish: Delete fish config
    This is barely fleshed out and I haven't touched it in ten years. It's
    diverged so much from my other shell configs that I'd have to redo it if
    I ever set up fish, anyway.

--- fish/.config/fish/functions/fish_greeting.fish
+++
@@ -1,7 +0,0 @@
-function fish_greeting
-    if test -r /etc/motd
-        lolcat </etc/motd
-    else
-        echo "(hostname -s)?" | cowthink | lolcat
-    end
-end
--- fish/.config/fish/functions/fish_prompt.fish
+++
@@ -1,7 +0,0 @@
-function fish_prompt
-    set_color blue
-    echo -n (basename (prompt_pwd))
-    set_color -o black
-    echo -n ' > '
-    set_color normal
-end
