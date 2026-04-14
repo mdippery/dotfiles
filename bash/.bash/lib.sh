181c3041ca68501ba6097716533b15e539eb69f3
Michael Dippery <michael@monkey-robot.com>
2026-04-13 18:30:52 -0700

    bash: Turn `onoe` into a function again
    It's incredibly likely I will eventually use this in my bash config
    script somewhere, or maybe I already do and I just missed it, so let's
    make it a function. Turns out we can easily export functions anyway.

--- bash/.bash/lib.sh
+++ bash/.bash/lib.sh
@@ -1,5 +1,10 @@
 # lib.sh: Common functions that many bash subscripts may find useful
 
+function onoe {
+  echo $* 1>&2
+}
+export -f onoe
+
 source "${DOTBASH}/lib/paths.sh"
 source "${DOTBASH}/lib/greet.sh"
 
