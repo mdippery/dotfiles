8d8c3600e81e3f24b41b9bc821a143d7e0d036b8
Michael Dippery <michael@monkey-robot.com>
2026-04-14 17:57:54 -0700

    bash: Put pushed dirs sigil first in prompt

--- bash/.bash/lib/prompt.sh
+++ bash/.bash/lib/prompt.sh
@@ -55,7 +55,7 @@ function _ps1_git_branch {
 }
 
 function _ps1 {
-  export PS1="$(_ps1_ssh_host)$(_ps1_prev_dir)$(_ps1_pushed_dirs)$(_ps1_virtual_env)$(_ps1_cwd)$(_ps1_git_branch)\n$(_ps1_sigil) "
+  export PS1="$(_ps1_ssh_host)$(_ps1_pushed_dirs)$(_ps1_prev_dir)$(_ps1_virtual_env)$(_ps1_cwd)$(_ps1_git_branch)\n$(_ps1_sigil) "
 }
 
 # vim: set ft=bash :
