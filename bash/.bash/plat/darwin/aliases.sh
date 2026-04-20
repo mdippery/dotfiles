da6ad9dfd0227fe221c272e516197335a40ea1f3
Michael Dippery <michael@monkey-robot.com>
2026-04-20 15:08:07 -0700

    bash: Store `fd` color scheme in ~/.config/fd
    This makes it a bit easier to change on the fly, and a bit easier to
    edit the actual colors.
    
    Install this package with
    
        $ stow fd

--- bash/.bash/plat/darwin/aliases.sh
+++ bash/.bash/plat/darwin/aliases.sh
@@ -1,6 +1,7 @@
 alias binld='blib'
 alias dec='otool -tvV -use-color'
 alias dis='dec'
-alias fd='LS_COLORS="$FD_COLORS" fd'
+alias fd='LS_COLORS="$(fdcolors)" fd'
+alias fdcolors="paste -sd : $XDG_CONFIG_HOME/fd/fdcolors"
 alias linux-sh='docker run --rm -it ubuntu:xenial'
 alias lsusb='system_profiler SPUSBDataType'
