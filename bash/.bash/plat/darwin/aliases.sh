2b004e55873d8f4422527b8d647949cee04f34c2
Michael Dippery <michael@monkey-robot.com>
2026-04-20 15:29:12 -0700

    bash: Only alias `fd` if original command is available

--- bash/.bash/plat/darwin/aliases.sh
+++ bash/.bash/plat/darwin/aliases.sh
@@ -1,7 +1,10 @@
 alias binld='blib'
 alias dec='otool -tvV -use-color'
 alias dis='dec'
-alias fd='LS_COLORS="$(fdcolors)" fd'
-alias fdcolors="paste -sd : $XDG_CONFIG_HOME/fd/fdcolors"
 alias linux-sh='docker run --rm -it ubuntu:xenial'
 alias lsusb='system_profiler SPUSBDataType'
+
+if command -v fd &>/dev/null; then
+  alias fd='LS_COLORS="$(fdcolors)" fd'
+  alias fdcolors="paste -sd : $XDG_CONFIG_HOME/fd/fdcolors"
+fi
