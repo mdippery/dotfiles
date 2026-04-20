b9bad348f7b9fd92dce0166029816962c3359ef7
Michael Dippery <michael@monkey-robot.com>
2026-04-20 13:07:31 -0700

    bash: `fd` should highlight block and character devices
    They were already a different color, but this change makes them use a
    different background instead of a different foreground.

--- bash/.bash/plat/darwin/environment.sh
+++ bash/.bash/plat/darwin/environment.sh
@@ -1,3 +1,3 @@
-export FD_COLORS='di=34:fi=00:ln=35:ex=31:ow=33;07:st=34;07:*.DS_Store=30;01:*.CFUserTextEncoding=30;01'
+export FD_COLORS='di=34:fi=00:ln=35:ex=31:ow=33;07:st=34;07:cd=96;07:bd=33;07:*.DS_Store=30;01:*.CFUserTextEncoding=30;01'
 export PIP_CONFIG_FILE="${XDG_CONFIG_HOME}/pip/pip.conf"
 export SHELL_SESSION_HISTORY=0
