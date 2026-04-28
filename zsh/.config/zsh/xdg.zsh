969363a9b33316899552aa8c6486f87a959f9064
Michael Dippery <michael@monkey-robot.com>
2026-04-28 16:39:06 -0700

    zsh: TODO: Figure out a way to share xdg.sh

--- zsh/.config/zsh/xdg.zsh
+++ zsh/.config/zsh/xdg.zsh
@@ -1,3 +1,11 @@
+# TODO: This is almost the same thing as ~/.bash/xdg.sh. I should really
+# figure out a way to share files like these between compatible shells,
+# maybe by storing this in ${XDG_DATA_HOME}/sh/xdg.sh.
+#
+# (While I'm at it, I should also move the files in ~/.bash to ~/.config/bash,
+# like I do for zsh, and maybe start storing appropriate files in a subdirectory
+# somewhere under ~/.local, too.)
+
 export XDG_BIN_HOME=${XDG_BIN_HOME:-$HOME/.local/bin}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
