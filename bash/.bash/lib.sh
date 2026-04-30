3d015c84d2cb2a86bc22907a1ca04c410b3e8a8f
Michael Dippery <michael@monkey-robot.com>
2026-04-30 09:29:05 -0700

    bash: Ditch the `onoe` function
    It's not portable across shells, and unlike bash, most shells don't have
    a facility for making shell functions available in scripts. I shouldn't
    use it in scripts.

--- bash/.bash/lib.sh
+++ bash/.bash/lib.sh
@@ -1,9 +1,4 @@
 # lib.sh: Common functions that many bash subscripts may find useful
 
-function onoe {
-  echo $* 1>&2
-}
-export -f onoe
-
 source "${DOTBASH}/lib/paths.sh"
 [ -r "${DOTBASH}/lib/greet.sh" ] && source "${DOTBASH}/lib/greet.sh"
