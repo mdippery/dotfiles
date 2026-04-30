f935290dbfc44a8c0c4d07e19a8d927801808651
Michael Dippery <michael@monkey-robot.com>
2026-04-30 09:57:05 -0700

    zsh: Store user config files in "user" subdirectory

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -6,3 +6,4 @@ export DOTFILES="${HOME}/.dotfiles"
 export VIMFILES="${HOME}/.vimfiles"
 
 [ -r "${DOTZSH_PLAT}/environment.zsh" ] && . "${DOTZSH_PLAT}/environment.zsh"
+[ -r "${DOTZSH}/user/environment.zsh" ] && . "${DOTZSH}/user/environment.zsh"
