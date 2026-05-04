693796817dfa99e15dc03c11fcf6431ac5a88436
Michael Dippery <michael@monkey-robot.com>
2026-05-04 09:23:52 -0700

    zsh: Set up environment in environment.zsh
    It's better to do the configuration there so I can add user files
    alongside the base environment.zsh, even though zsh has its own separate
    ~/.zshenv file, unlike bash.
    
    I will continue to do some very basic initialization in ~/.zshenv that
    is needed by all of zsh.

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -1,6 +1,23 @@
-# environment.zsh mirrors my long-standing bash setup, but it might make
-# more sense to merge this file with ~/.zshenv, since zsh already has its
-# own separate environment file.
+##############################################################################
+# Check /etc/zshrc when configuring zsh, especially on macOS.
+# It can set some gnarly options.
+# It also sets some environment variables which should be really be set
+# in /etc/zshenv, which means they have to be overridden or unset in
+# ~/.zshrc, since /etc/zshrc is sourced after ~/.zshenv. Bad, macOS!
+#
+#
+# In particular, I might want to set $HISTFILE to $ZDG_STATE_HOME/zsh,
+# like my other history files.
+#
+# Or if I want to disable it entirely, run `unset HISTFILE` as in bash.
+# Lots of other great options to look at, like $HIST_IGNORE_SPACE and
+# $HIST_IGNORE_DUPS; see `man zshoptions` for more settings.
+##############################################################################
+
+# $TERM is not always set in zsh -- make sure we set it early.
+[ -z "$TERM" ] && export TERM='xterm-256color'
+
+. "${DOTZSH}/xdg.zsh"
 
 export DOTFILES="${HOME}/.dotfiles"
 export VIMFILES="${HOME}/.vimfiles"
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -6,26 +6,10 @@
 # in the root of the home folder.
 ##############################################################################
 
-# Check /etc/zshrc when configuring zsh, especially on macOS.
-# It can set some gnarly options.
-# It also sets some environment variables which should be really be set
-# in /etc/zshenv, which means they have to be overridden or unset in
-# ~/.zshrc, since /etc/zshrc is sourced after ~/.zshenv. Bad, macOS!
-#
-# In particular, I might want to set $HISTFILE to $ZDG_STATE_HOME/zsh,
-# like my other history files.
-# Or if I want to disable it entirely, run `unset HISTFILE` as in bash.
-# Lots of other great options to look at, like $HIST_IGNORE_SPACE and
-# $HIST_IGNORE_DUPS; see `man zshoptions` for more settings.
 
 export DOTZSH="${HOME}/.config/zsh"
 export OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 export DOTZSH_PLAT="${DOTZSH}/plat/${OS}"
 #export ZDOTDIR=$DOTZSH
-
-# $TERM is not always set in zsh -- make sure we set it early.
-[ -z "$TERM" ] && export TERM='xterm-256color'
-
-. "${DOTZSH}/xdg.zsh"
 
 . "${DOTZSH}/environment.zsh"
