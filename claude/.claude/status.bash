d464f42be2f3a463570cb646e1ccf2d80bfc115e
Michael Dippery <michael@monkey-robot.com>
2026-06-12 08:46:53 -0700

    claude: Show cost in prompt
    Now that I am using Fable, I love to see how much of my company's AI
    budget I am burning through.

--- claude/.claude/status.bash
+++ claude/.claude/status.bash
@@ -9,18 +9,22 @@ reset=$(tput sgr0)
 
 sep=" • "
 
-cwd=$(basename $(echo "$input" | jq -r .workspace.current_dir))
+# cwd=$(basename $(echo "$input" | jq -r .workspace.current_dir))
+project_dir=$(basename $(echo "$input" | jq -r .workspace.project_dir))
 model=$(echo "$input" | jq -r .model.display_name)
 usage=$(echo "$input" | jq -r .rate_limits.five_hour.used_percentage)
+cost=$(printf "$%.2f" $(echo "$input" | jq -r .cost.total_cost_usd))
 branch=$(git branch --show-current)
 
 # Project directory and Git branch
-echo -n "📂 $cwd"
+echo -n "📂 $project_dir"
 echo -n "$sep"
 echo -n "${yellow} $branch${reset}"
 
 # Quota and model
 if [ $(tput cols) -gt 100 ]; then echo -n "$sep"; else echo; fi
 echo -n "🤖 ${teal}${model}${reset}"
+echo -n "$sep"
+echo -n "💸 ${cost}"
 # echo -n "$sep"
 # echo -n "⏱️ ${blue}${usage}%${reset}"
